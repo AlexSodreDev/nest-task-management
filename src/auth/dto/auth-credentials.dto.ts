a005c17d0291d64a8d2debe01054156713605b26
AlexSodreDev <alexandre.cortines@gmail.com>
2022-05-11 11:02:16 -0300

    add validations on authCredentialsDto

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,24 @@
+import {
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password must have numbers, letters and special characters',
+  })
+  password: string;
+}
